07e5e0ef45b0da2992b96e9637a5a5064ed08cb3
Jicheol Oh <jicheol@gmail.com>
2021-05-24 17:08:40 -0700

    Projecting Data with Select

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -94,7 +94,39 @@ namespace Cars
                 where line.Length > 1
                 select Car.ParseFromCsv(line);
 
+            var query2 =
+
+                File.ReadAllLines(path)
+                    .Skip(1)
+                    .Where(l => l.Length > 1)
+                    .ToCar();
+
             return query.ToList();
+        }
+
+
+    }
+
+    public static class CarExtensions
+    {
+        public static IEnumerable<Car> ToCar(this IEnumerable<string> source)
+        {
+            foreach (var line in source)
+            {
+                var columns = line.Split(',');
+
+                yield return new Car
+                {
+                    Year = int.Parse(columns[0]),
+                    Manufacturer = columns[1],
+                    Name = columns[2],
+                    Displacement = double.Parse(columns[3]),
+                    Cylinders = int.Parse(columns[4]),
+                    City = int.Parse(columns[5]),
+                    Highway = int.Parse(columns[6]),
+                    Combined = int.Parse(columns[7])
+                };
+            }
         }
     }
 }
