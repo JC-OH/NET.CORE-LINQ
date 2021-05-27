e3d2832c6250921b79d763f62470554a8b24b9e4
Jicheol Oh <jicheol@gmail.com>
2021-05-27 11:15:48 -0700

    Functional Construction for Less Code

--- XML/Program.cs
+++ XML/Program.cs
@@ -13,6 +13,7 @@ namespace XML
             var records = ProcessCars("fuel.csv");
             //Console.WriteLine(records.GetType());
 
+            //==================================================================
             var document = new XDocument();
             var cars = new XElement("Cars");
 
@@ -29,6 +30,53 @@ namespace XML
             document.Add(cars);
             document.Save("fuel1.xml");
             //==================================================================
+            document = new XDocument();
+            cars = new XElement("Cars");
+            foreach (var record in records)
+            {
+                /*var car = new XElement("Car");
+                var name = new XAttribute("Name", record.Name);
+                var combined = new XAttribute("Combined", record.Combined);
+                car.Add(name);
+                car.Add(combined);*/
+
+                /*var name = new XAttribute("Name", record.Name);
+                var combined = new XAttribute("Combined", record.Combined);
+                var car = new XElement("Car", name, combined);*/
+
+                var car = new XElement("Car", 
+                                        new XAttribute("Name", record.Name), 
+                                        new XAttribute("Combined", record.Combined),
+                                        new XAttribute("Manufacterer", record.Manufacturer)
+                                       );
+
+                cars.Add(car);
+            }
+            document.Add(cars);
+            document.Save("fuel2.xml");
+            //==================================================================
+            document = new XDocument();
+            /*cars = new XElement("Cars");
+            var elements =
+                    from record in records
+                    select new XElement("Car",
+                                        new XAttribute("Name", record.Name),
+                                        new XAttribute("Combined", record.Combined),
+                                        new XAttribute("Manufacterer", record.Manufacturer)
+                                       );
+            cars.Add(elements);*/
+
+            cars = new XElement("Cars", 
+                from record in records
+                select new XElement("Car",
+                                    new XAttribute("Name", record.Name),
+                                    new XAttribute("Combined", record.Combined),
+                                    new XAttribute("Manufacterer", record.Manufacturer)
+                                    ));
+            document.Add(cars);
+            document.Save("fuel3.xml");
+
+
         }
 
         private static List<Car> ProcessCars(string path)
