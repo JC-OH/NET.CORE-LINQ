9f468c33c202142848f8dd0b283e15fd23bdad95
Jicheol Oh <jicheol@gmail.com>
2021-06-03 08:50:33 -0700

    Connecting DB with .NET Core

---
+++ EntityFrameworkCore/CarDb.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkCore
+{
+    public class CarDb : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .EnableSensitiveDataLogging()
+                .UseSqlServer(
+                "Data Source = localhost; Initial Catalog=LINQEF; Persist Security Info=True;User ID=test; Password=test;");
+        }
+    }
+}
---
+++ EntityFrameworkCore/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace EntityFrameworkCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new CarDb())
+            {
+                var query =
+                    db.Cars.Where(c => c.Manufacturer == "BMW")
+                            .Select(c => new
+                            {
+                                Name = c.Name
+                            });
+                foreach (var car in query.ToList())
+                {
+                    Console.WriteLine($"{car.Name}");
+                }
+            }
+        }
+    }
+}
