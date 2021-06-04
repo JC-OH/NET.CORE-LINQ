bb8faab5dd0fc20d4ac2b25af5eaa86dfe43ac37
Jicheol Oh <jicheol@gmail.com>
2021-06-04 11:55:10 -0700

    Add Configuration to a .Net Core Console Application

---
+++ EntityFrameworkCore/LoggingConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkCore
+{
+    public class LoggingConfig
+    {
+        public string LogPath { get; set; }
+        public string Level { get; set; }
+    }
+}
--- EntityFrameworkCore/Program.cs
+++ EntityFrameworkCore/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Linq;
 
 namespace EntityFrameworkCore
@@ -17,9 +18,39 @@ namespace EntityFrameworkCore
                             });
                 foreach (var car in query.ToList())
                 {
-                    Console.WriteLine($"{car.Name}");
+                    //Console.WriteLine($"{car.Name}");
                 }
             }
+
+            Console.WriteLine("============================================");
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
+
+            Console.WriteLine(configuration["Message"]);
+            var testOPT = configuration.GetSection("TestOPT");
+            Console.WriteLine(testOPT["id"]);
+
+            string connectionString = configuration.GetValue<string>("ServiceBusConnectionString");
+
+            Console.WriteLine(connectionString);
+
+            Console.WriteLine("============================================");
+            var loggingConfig = new LoggingConfig();
+            configuration.Bind("Logging", loggingConfig);
+
+            Console.WriteLine(loggingConfig.LogPath);
+            Console.WriteLine(loggingConfig.Level);
+
+            Console.WriteLine("============================================");
+
+            /*IConfiguration configuration = new ConfigurationBuilder()
+                            .AddJsonFile("appsettings.json", true, true)
+                            .AddUserSecrets<Program>()
+                            .Build();*/
+
         }
     }
 }
