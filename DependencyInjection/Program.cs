dd19c8f91bd504c7dc913e10c27f6c09c5499fb8
Jicheol Oh <jicheol@gmail.com>
2021-06-25 22:16:05 -0700

    ConsoleAppAssembly

--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -3,6 +3,8 @@ using DependencyInjection.Implementation;
 using Microsoft.Extensions.DependencyInjection;
 
 using System;
+using System.Linq;
+using System.Reflection;
 
 namespace DependencyInjection
 {
@@ -24,8 +26,24 @@ namespace DependencyInjection
         private static void ResigerServices()
         {
             var services = new ServiceCollection();
+            Console.WriteLine("================================");
             services.AddSingleton<ICustomer, Customer>();
             services.AddSingleton<ConsoleApplication>();
+            
+            Console.WriteLine("================================");
+            Assembly ConsoleAppAssembly = typeof(Program).Assembly;
+
+            var ConsoleAppTypes =
+                from type in ConsoleAppAssembly.GetTypes()
+                where !type.IsAbstract
+                where typeof(ICustomer).IsAssignableFrom(type)
+                select type;
+            foreach (var type in ConsoleAppTypes)
+            {
+                Console.WriteLine(type.FullName);
+                services.AddTransient(typeof(ICustomer), type);
+            }
+            Console.WriteLine(ConsoleAppTypes.Count());
 
             _serviceProvider = services.BuildServiceProvider(true);
         }
